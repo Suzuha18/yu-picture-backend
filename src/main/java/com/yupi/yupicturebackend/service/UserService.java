d8be4268f43374d363a430fc08b657803a94729d
Suzuha18 <13761259008@163.com>
2025-07-02 00:42:16 +0800

    Initial commit

---
+++ src/main/java/com/yupi/yupicturebackend/service/UserService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import generator.domain.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author SUZURI
+* @description 针对表【user(用户)】的数据库操作Service
+* @createDate 2025-07-01 14:55:10
+*/
+public interface UserService extends IService<User> {
+
+}
